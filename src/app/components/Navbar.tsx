c8307763885cee61c462d25da6ff925630ad4d74
Sumeet Sumeet <Sumeet5990@conestogac.on.ca>
2024-10-08 14:22:13 -0400

    Clerk setup done

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import MobileMenu from "./MobileMenu";
 import Image from 'next/image';
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from '@clerk/nextjs';
+
 
 
 const Navbar =() =>{
@@ -8,10 +11,10 @@ const Navbar =() =>{
        <div className='h-24 flex items-center justify-between'>
         {/* Left */}
         <div className="md:hidden lg:block w-[20%]">
-            <Link href="/" className="font-bold text-xl text-blue-600"><img src="/logo_connectiFy.png" alt="Logo of App" /></Link>
+            <Link href="/" className="font-bold text-xl "><img src="/logo_connectify.png" alt="" /></Link>
         </div>
         {/* Center */}
-        <div className="hidden md:flex w-[50%] text-sm">
+        <div className="hidden md:flex w-[50%] text-sm ml-10 items-center justify-between">
         {/* Links */}
         <div className="flex gap-6 text-gray-600 ">
             <Link href="/" className="flex items-center gap-2">
@@ -30,12 +33,37 @@ const Navbar =() =>{
             </Link>
         </div>
         
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+            <input type="text" placeholder="Search..." className="bg-transparent outline-none"/>
+            <Image src="/search.png" alt="" width={14} height={14} />
+        </div>
 
         </div>
         {/* Right */}
-        <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
-            <MobileMenu/>
+        <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end mr-4">
+            
+        <ClerkLoading>
+        <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+  role="status"></div>
+            </ClerkLoading>
+            <ClerkLoaded>
+                <SignedIn><div className="flex items-center gap-4 ">
+                    <div className="cursor-pointer"><Image src="/people.png" alt="" width={24} height={24} /></div>
+                    <div className="cursor-pointer"><Image src="/messages.png" alt="" width={24} height={24} /></div>
+                    <div className="cursor-pointer"><Image src="/notifications.png" alt="" width={24} height={24} /></div>
+                    <UserButton/>
+                    <MobileMenu/>
+                    </div>
+                    </SignedIn>
 
+               <SignedOut><div className="flex items-center gap-2 mr-4">
+                    <Image src="/login.png" alt="" width={24} height={24}/>
+                    <Link href="/sign-in" >Login/Register</Link>
+                    </div>
+                </SignedOut>
+            </ClerkLoaded>    
+            
+            
         </div>
        </div>
     )
